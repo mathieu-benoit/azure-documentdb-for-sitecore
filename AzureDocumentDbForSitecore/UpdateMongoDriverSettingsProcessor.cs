1c79aac8f83c9146acb1bd4e269bf20e741da2c4
mathieu-benoit <mathieu-benoit@hotmail.fr>
2016-10-18 20:13:40 -0700

    Initial commit to override the default mongodriver.

---
+++ AzureDocumentDbForSitecore/UpdateMongoDriverSettingsProcessor.cs
@@ -0,0 +1,19 @@
+﻿using Sitecore.Analytics.Pipelines.UpdateMongoDriverSettings;
+using System.Security.Authentication;
+
+namespace AzureDocumentDBForSitecore
+{
+    public class UpdateMongoDriverSettingsProcessor : global::Sitecore.Analytics.Pipelines.UpdateMongoDriverSettings.UpdateMongoDriverSettingsProcessor
+    {
+        public override void UpdateSettings(UpdateMongoDriverSettingsArgs args)
+        {
+            if (args.MongoSettings.Server != null
+                && !string.IsNullOrEmpty(args.MongoSettings.Server.Host)
+                && args.MongoSettings.Server.Host.EndsWith(".documents.azure.com"))
+            {
+                args.MongoSettings.SslSettings = new MongoDB.Driver.SslSettings();
+                args.MongoSettings.SslSettings.EnabledSslProtocols = SslProtocols.Tls12;
+            }
+        }
+    }
+}
